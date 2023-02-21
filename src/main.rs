d0de47fddead0364b5d036d5173bdfead05ba091
eaterofpies <eaterofpies@users.noreply.github.com>
2023-02-21 21:00:14 +0000

    Make the device list easier to read

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,21 @@ use std::error::Error;
 use futures::stream::{FuturesUnordered,StreamExt};
 use std::collections::HashMap;
 
+fn print_list_item(path: &str, phy_path: &str, name: &str){
+    println!("| {0: <20} | {1:<30} | {2:}",path, phy_path, name)
+}
+
 fn list_dev_paths(){
     let devices = evdev::enumerate().collect::<HashMap<_,_>>();
     // readdir returns them in reverse order from their eventN names for some reason
+    print_list_item("path", "physical path", "name");
+    print_list_item("--------------------", "------------------------------", "----");
+
     for path_and_dev in devices.iter() {
         let path = path_and_dev.0;
         let dev = path_and_dev.1;
-        println!("path: {:?} physical path: \"{}\" name: \"{}\"",
-            path.as_os_str(),
+        print_list_item(
+            path.as_os_str().to_string_lossy().as_ref(),
             dev.physical_path().unwrap_or("Unknown Path") ,
             dev.name().unwrap_or("Unnamed device")
         );
